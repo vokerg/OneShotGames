d033943ac7b7d824ea6946cb56b2692199f2e7ca
vokerg <vokerg@gmail.com>
2026-08-08 12:24:01 +0200

    art(ufrts): add Russian vehicle runtime installer

---
+++ ukrainian-front-rts/src/render/russian-vehicle-runtime-install.js
@@ -0,0 +1,4 @@
+import { Renderer } from '../render.js';
+import { installRussianVehicleArtPass } from './russian-vehicle-art-pass.js';
+
+export const russianVehicleArtInstallation = installRussianVehicleArtPass(Renderer);
